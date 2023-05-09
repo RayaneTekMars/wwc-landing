2944a5a9908e4ce5ff3070e5714992d5a82289e9
Rayane ELOUDJEDI <rayane.eloudjedi@epitech.eu>
2023-05-09 17:24:17 +0200

    feat: terms of services and policy

---
+++ src/pages/tos/index.tsx
@@ -0,0 +1,48 @@
+import { Typography } from "@mui/material";
+import Head from "next/head";
+import styles from "../../styles/Policies.module.scss";
+import { Raleway } from "next/font/google";
+import Header from "@/components/Header";
+
+const raleway = Raleway({ subsets: ["latin"] });
+
+export default function Policies() {
+  return (
+    <div>
+      <Head>
+        <title>Terms of Service</title>
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Header />
+        <div className={styles.tos_title_container}>
+          <Typography className={`${raleway.className} ${styles.tos_title}`}>
+            Terms of Service
+          </Typography>
+        </div>
+        <div className={styles.tos_text_container}>
+          <iframe
+            src="tos.html"
+            width="100%"
+            height="500"
+            style={{ border: "none", borderRadius: "5px" }}
+          />
+        </div>
+        <div className={styles.pol_title_container}>
+          <Typography className={`${raleway.className} ${styles.tos_title}`}>
+            Private policy
+          </Typography>
+        </div>
+        <div className={styles.pol_text_container}>
+          <iframe
+            src="policy.html"
+            width="100%"
+            height="500"
+            style={{ border: "none", borderRadius: "5px" }}
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
